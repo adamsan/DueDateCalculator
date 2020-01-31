4592c39b588b3e041fb60233adf837a8f99fbcf7
Adam Szentirmai-Schon <adamsan3@gmail.com>
2020-02-01 00:05:57 +0100

    Refactor: introduce constants in validator

--- src/main/java/com/emarsys/homework/DueDateCalculatorValidator.java
+++ src/main/java/com/emarsys/homework/DueDateCalculatorValidator.java
@@ -1,13 +1,19 @@
 package com.emarsys.homework;
 
+import java.time.DayOfWeek;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
-import java.util.stream.Stream;
+import java.util.Arrays;
+import java.util.List;
 
 import static java.time.DayOfWeek.SATURDAY;
 import static java.time.DayOfWeek.SUNDAY;
 
 public class DueDateCalculatorValidator {
+    private static final LocalTime START_WORK_TIME = LocalTime.of(9, 0);
+    private static final LocalTime END_WORK_TIME = LocalTime.of(17, 0);
+    private static final List<DayOfWeek> WEEKEND_DAYS = Arrays.asList(SATURDAY, SUNDAY);
+
     public void validateTurnaroundHours(int turnaroundHours) {
         if (turnaroundHours < 0) {
             throw new IllegalArgumentException("Turnaround hours can't be negative!");
@@ -27,13 +33,11 @@ public class DueDateCalculatorValidator {
     }
 
     private boolean isWeekend(LocalDateTime submission) {
-        return Stream.of(SATURDAY, SUNDAY).anyMatch(day -> day == submission.getDayOfWeek());
+        return WEEKEND_DAYS.stream().anyMatch(day -> day == submission.getDayOfWeek());
     }
 
     private boolean isWorkingTime(LocalDateTime submission) {
-        LocalTime start = LocalTime.of(9, 0);
-        LocalTime end = LocalTime.of(17, 0);
         LocalTime submissionTime = submission.toLocalTime();
-        return submissionTime.equals(start) || submissionTime.isAfter(start) && submissionTime.isBefore(end);
+        return submissionTime.equals(START_WORK_TIME) || submissionTime.isAfter(START_WORK_TIME) && submissionTime.isBefore(END_WORK_TIME);
     }
 }
